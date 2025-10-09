0832db093ce0901e156e1e52c8002466b428b101
will <wilsonrfros@gmail.com>
2025-10-09 17:01:31 -0300

    update

--- utils/apiClients.js
+++
@@ -1,8 +0,0 @@
-const TruckyServicesClient = require('@dowmeister/trucky-services-client');
-
-const trucky = new TruckyServicesClient();
-console.log('Inspeccionando objeto trucky:', trucky);
-
-module.exports = {
-    trucky,
-};
